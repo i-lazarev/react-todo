bf1b15cffdb4f7ed3c8ffb2451b2a98345a70fc6
i-lazarev <vanealazarev@gmail.com>
2020-01-07 11:25:09 +0100

    ...

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -5,7 +5,7 @@ export default function Counter(props) {
     const x = items.filter(item=>item.status===true)
     const y = items.length
     return (
-        <div>
+        <div className="space">
             Task completed <span className="done">{x.length}</span>  from <span className="undone">{y}</span>
         </div>
     )
