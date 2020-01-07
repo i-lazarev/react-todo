40d7339e113224c87514a57915b3c3790be364c0
i-lazarev <vanealazarev@gmail.com>
2020-01-07 11:31:40 +0100

    ...

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React from "react";
 
 export default function Counter(props) {
-    const items = props.items
-    const x = items.filter(item=>item.status===true)
-    const y = items.length
-    return (
-        <div className="space">
-            Task completed <span className="done">{x.length}</span>  from <span className="undone">{y}</span>
-        </div>
-    )
+  const items = props.items;
+  const x = items.filter(item => item.status === true);
+  const y = items.length;
+  return (
+    <div className="space">
+      Task completed <span className="badge badge-success">{x.length}</span> from{" "}
+      <span className="badge badge-danger">{y}</span>
+    </div>
+  );
 }
